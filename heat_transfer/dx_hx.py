70a668e132282a88e72f6d727d9d9a111cd5a64d
Saif Aqel <saifaqel3@gmail.com>
2025-09-26 02:12:23 +0300

    dx_hx

---
+++ heat_transfer/dx_hx.py
@@ -0,0 +1,60 @@
+from math import log, pi
+
+class WaterResistance:
+    def __init__(self, h_boil):
+        self.h_boil = h_boil
+
+    def resistance_per_area(self):
+        return 1 / self.h_boil
+
+
+class GasResistance:
+    def __init__(self, h_rad, h_conv):
+        self.h_rad = h_rad
+        self.h_conv = h_conv
+
+    def resistance_per_area(self):
+        return 1 / (self.h_rad + self.h_conv)
+
+
+class WallResistance:
+    def __init__(self, r_i, r_o, k):
+        self.r_i = r_i
+        self.r_o = r_o
+        self.k = k
+
+    def cyl_per_inner_area(self):
+        return log(self.r_o / self.r_i) / (2 * pi * self.k * self.r_i)
+
+
+class FoulingResistance:
+    def __init__(self, thickness, k_foul):
+        self.thickness = thickness
+        self.k_foul = k_foul
+
+    def resistance_per_area(self):
+        return self.thickness / self.k_foul
+
+
+class TotalResistance:
+    def __init__(self, r_water, r_wall, r_gas, r_foul_inner, r_foul_outer):
+        self.r_water = r_water
+        self.r_wall = r_wall
+        self.r_gas = r_gas
+        self.r_foul_inner = r_foul_inner
+        self.r_foul_outer = r_foul_outer
+
+    def resistance_per_area(self):
+        return (self.r_water + self.r_wall + self.r_gas + self.r_foul_inner + self.r_foul_outer)
+
+class Flux:
+    def __init__(self, delta_T, r_total_per_area):
+        self.delta_T = delta_T
+        self.r_total_per_area = r_total_per_area
+
+    def resistance_per_area(self):
+        return self.delta_T / self.r_total_per_area
+
+class HeatTransfer:
+    def compute(self, flux, area):
+        return flux * area
